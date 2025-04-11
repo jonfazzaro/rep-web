070e9b5e4a913cc92760e1596452b656226cd542
Jon Fazzaro <jon@fazzaro.com>
2025-04-11 12:26:39 -0400

    hello vitest

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig, UserConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/vitest.setup.ts',
+    },
+} as UserConfig);
