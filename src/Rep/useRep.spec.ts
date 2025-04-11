06d19af0e93f8034bd775d823e94ebb261e74aef
Jon Fazzaro <jon@fazzaro.com>
2025-04-11 12:41:15 -0400

    feat: scaffold tests based on Rep mobile app

---
+++ src/Rep/useRep.spec.ts
@@ -0,0 +1,53 @@
+describe('The Rep hook', () => {
+    it('is a view model', () => {
+        expect.fail()
+    });
+
+    it.skip('has a count', () => {
+        expect.fail()
+    });
+
+    it.skip('has a start and end time', () => {
+        expect.fail()
+    });
+
+    it.skip('has a \'has started\' flag', () => {
+        expect.fail()
+    });
+
+    describe('when repping', () => {
+        it.skip('increments the count', () => {
+            expect.fail()
+        });
+
+        it.skip('sets the start time given it\'s the first time', () => {
+            expect.fail()
+        });
+
+        it.skip('sets the \'has started\' flag', () => {
+            expect.fail()
+        });
+
+        describe('and then resetting', () => {
+            it.skip('resets everything', () => {
+                expect.fail()
+            });
+
+            describe('and then saving', () => {
+                it.skip('saves to the store', () => {
+                    expect.fail()
+                });
+
+                it.skip('resets everything', () => {
+                    expect.fail()
+                });
+            });
+        });
+    });
+
+    describe('when saving before repping', () => {
+        it.skip('does not save to the store', () => {
+            expect.fail()
+        });
+    });
+});
