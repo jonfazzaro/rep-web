13427507d1af8882a76857ff869f9554c0fa9533
Jon Fazzaro <jon@fazzaro.com>
2025-04-14 14:59:04 -0400

    style

--- src/Rep/Rep.tsx
+++ src/Rep/Rep.tsx
@@ -1,10 +1,10 @@
 import {useRep} from "./useRep.ts";
 
 export function Rep() {
-    const {count, rep, save, reset} = useRep()
+    const {count, rep, save, reset, hasStarted} = useRep()
     return <div className='rep'>
-        <button onClick={reset}>Reset</button>
-        <button onClick={save}>Save</button>
+        <button className='reset' onClick={reset}>Reset</button>
+        <button className='save' onClick={save} disabled={!hasStarted}>Save</button>
         <button className='count' onClick={rep}>{count}</button>
     </div>
 }
